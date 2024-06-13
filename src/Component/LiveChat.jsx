cf06455c54f0da23de0eb0dcc7322a1b9cdf8a71
Akriti <akritikohli05@gmail.com>
2024-06-13 12:37:49 +0530

    created and dispached liveChat input box

--- src/Component/LiveChat.jsx
+++ src/Component/LiveChat.jsx
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ChatMsgs from "./ChatMsgs"
 import { useDispatch, useSelector } from "react-redux";
 import { addMsg } from "../utils/chatSlice";
 import { generate, randomMessages } from "../utils/helper";
 const LiveChat = () => {
+
+  const [liveMsg, setLiveMsg] = useState("");
 
   const dispatch = useDispatch();
   const chatMessages = useSelector((store) => store.chatMsgs. messages);
@@ -12,7 +14,6 @@ const LiveChat = () => {
    
     const i =setInterval(() => {
       //api polling
-      console.log("Polling");
       dispatch( addMsg({
         name: generate(),
         message:randomMessages(20)
@@ -22,14 +23,34 @@ const LiveChat = () => {
   })
   
   return (
-    <div className="w-full border-gray-400  bg-slate-50 rounded-lg  border m-1 text-xs h-[450px] overflow-y-scroll no-scrollbar flex flex-col-reverse">
-      
-      {chatMessages.map(c => <ChatMsgs
-       key={c.index}
-       name ={c.name}
-       messages={c.message}
-      />)}
-    </div>
+    <>
+      <div className="w-full border-gray-400  bg-slate-50 rounded-lg  border m-1 text-xs h-[450px] overflow-y-scroll no-scrollbar flex flex-col-reverse">
+        
+        {chatMessages.map(c => <ChatMsgs
+        key={c.index}
+        name ={c.name}
+        messages={c.message}
+        />)}
+      </div>
+
+      <form className="w-full m-1 rounded-lg border border-gray-400 py-1" onSubmit={(e) => {
+        e.preventDefault();
+        console.log("form submitted!") , liveMsg
+        dispatch(
+          addMsg({
+            name: "aimer aie",
+            message: liveMsg,
+          })
+        );
+        setLiveMsg("");
+        }}>
+        <input className=" text-sm w-64 rounded-tl-lg rounded-bl-lg px-2 outline-none" type="text" placeholder="Live Chat" value={liveMsg} onChange={(e) => {setLiveMsg(e.target.value)}}
+          />
+
+        <button className=" w-16 rounded-md border border-gray-300 text-gray-500 text-sm ">Send</button>
+
+      </form>
+    </>
   )
 }
 
